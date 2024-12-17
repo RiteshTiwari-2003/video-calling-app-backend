36f6aef437b5c302788befc88c6c1783d1a2fb9d
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-18 02:42:51 +0530

    this is my video call 5 commit

--- src/handlers/RoomHandler.ts
+++ src/handlers/RoomHandler.ts
@@ -1,8 +1,21 @@
-import { Socket } from "socket.io"
-const roomHandler=(socket:Socket)=>{
-    const createRoom=()=>{
-        
-    }
+import { Socket } from "socket.io";
+import {v4 as UUIDv4} from "uuid";
+const roomHandler = (socket: Socket) => {
+    const createRoom = () => {
+        const roomid=UUIDv4();//this will be our unique room id in which multiple connection will exchange data 
+        socket.join(roomid);// we will make the socket connection enter a new room 
+        socket.emit("room-created",{roomid});// we will emit a event from server side that socket connection has been added to a room 
 
-}
+    };
+    const joinRoom=()=>{
+        console.log("New room joined ");
+    }
+    //when to call above two function 
+    // we wil call the above two function when the client will emit events to create room and join room 
+    socket.on("create-room",createRoom);
+    socket.on("join-room",joinRoom);
+
+
+
+};
 export default roomHandler;
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import http from "http";
 import {Server} from "socket.io";
 import ServerConfig from "./config/serverConfig";
+import roomHandler from "./handlers/RoomHandler";
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
@@ -14,6 +15,7 @@ const io = new Server(server, {
 });
 io.on("connection", (socket) => {
     console.log("new user connected");
+    roomHandler(socket);//pass the socket connection to the room handler for room creation and join 
     socket.on("disconnect", () => {
         console.log("User Disconnected");
     });
